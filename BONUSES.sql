28b0787f0a99ac900609faf28343fd16a8c21a3b
ZakariaLabban <zbl00@mail.aub.edu>
2025-01-17 08:24:41 +0200

    Update and rename BONUSES to BONUSES.sql

--- BONUSES.sql
+++ BONUSES.sql
@@ -1,7 +1,7 @@
---Tia
-Recursive Query: Track Borrowing Chains for a Book
 
-Track the borrowing history of a specific book (BookID), showing a chain of borrowers who checked out the book sequentially.
+--Recursive Query: Track Borrowing Chains for a Book
+
+--Track the borrowing history of a specific book (BookID), showing a chain of borrowers who checked out the book sequentially.
 
 
 WITH RECURSIVE Borrowing_Chain AS (
@@ -51,28 +51,28 @@ ORDER BY Chain_Level, Date_Out;
 
 
 
-Explanation:
-Base Case:
-The query starts with the first borrower of the book (BookID). 
-It fetches the borrower's information along with the borrowing date (Date_Out) and due date (Due_Date).
-The LIMIT 1 ensures it starts with the first recorded borrower of the book.
-Recursive Case:
-The query then recursively finds subsequent borrowers who checked out the same book after it was returned.
-It uses the condition next_borrower.Date_Out > bc.Due_Date to find borrowers who checked out the book after it was previously due.
-Final Output:
-The recursive query builds a chain of borrowers, showing who borrowed the book, when they borrowed it, and if any penalties were incurred.
-The chain level (Chain_Level) shows the sequence in which the book was borrowed.
+--Explanation:
+--Base Case:
+--The query starts with the first borrower of the book (BookID). 
+--It fetches the borrower's information along with the borrowing date (Date_Out) and due date (Due_Date).
+--The LIMIT 1 ensures it starts with the first recorded borrower of the book.
+--Recursive Case:
+--The query then recursively finds subsequent borrowers who checked out the same book after it was returned.
+--It uses the condition next_borrower.Date_Out > bc.Due_Date to find borrowers who checked out the book after it was previously due.
+--Final Output:
+--The recursive query builds a chain of borrowers, showing who borrowed the book, when they borrowed it, and if any penalties were incurred.
+--The chain level (Chain_Level) shows the sequence in which the book was borrowed.
 
-Importance:
-This query is helpful for analyzing book demand and circulation within the library.
-It helps the library identify popular books that may need more copies due to high demand.
+--Importance:
+--This query is helpful for analyzing book demand and circulation within the library.
+--It helps the library identify popular books that may need more copies due to high demand.
 
 Usage:
 SELECT * FROM Borrowing_Chain;
 
 
 
-SECURITY MECHANISM 1: User Roles and Permissions
+--SECURITY MECHANISM 1: User Roles and Permissions
 -- Creating roles
 CREATE ROLE Librariann;
 CREATE ROLE Managerr;
@@ -91,7 +91,7 @@ GRANT SELECT ON Books_for_Sale, Books_for_Rent TO Customerr;
 
 
 
-SECURITY MECHANISM 2: Restrict data access based on the user’s role or identity
+--SECURITY MECHANISM 2: Restrict data access based on the user’s role or identity
 -- Enable row-level security on the Borrows table
 ALTER TABLE Borrows ENABLE ROW LEVEL SECURITY;
 
@@ -104,20 +104,19 @@ USING (Username = CURRENT_USER);
 ALTER TABLE Borrows FORCE ROW LEVEL SECURITY;
 
 
-!!!!!!!SO IMPORTANTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT CYBERSECURITY INTERNSHIP
 
-SECURITY MECHANISM 3: SQL INJECTION
+--SECURITY MECHANISM 3: SQL INJECTION
 
-SQLL INJECTION IS WHEN TRYING TO INSERT:
-SELECT * FROM Customer WHERE Username = 'en01' OR '1'='1';
+--SQLL INJECTION IS WHEN TRYING TO INSERT:
+--SELECT * FROM Customer WHERE Username = 'en01' OR '1'='1';
 
-QUERY:
+--QUERY:
 PREPARE find_customer (VARCHAR) AS
 SELECT * FROM Customer WHERE Username = $1;
 EXECUTE find_customer('en01');
 
 
-Security Mechanism 4: Encryption and Decryption
+--Security Mechanism 4: Encryption and Decryption
 
 CREATE EXTENSION pgcrypto;
 ALTER TABLE Authentication_System DROP CONSTRAINT chk_passcode;
@@ -125,10 +124,10 @@ ALTER TABLE Authentication_System
 ALTER COLUMN Passcode TYPE BYTEA USING pgp_sym_encrypt(Passcode::TEXT, 's3cUr3!kEy#2023@P0stgreSQL^');
 
 
-Encryption:
+--Encryption:
 Insert into Authentication_System Values('myh17@gmail.com',pgp_sym_encrypt('Qwerty123#@', 's3cUr3!kEy#2023@P0stgreSQL^'));
 
-Decryption:
+--Decryption:
 SELECT Email,
        pgp_sym_decrypt(Passcode, 's3cUr3!kEy#2023@P0stgreSQL^') AS Decrypted_Passcode
 FROM Authentication_System
